5bfdbf5ef9d9dc17ce8fa1a9e402a652b2a767f0
iamyelex <yelex4love@gmail.com>
2023-02-08 08:22:45 +0800

    chatbox initiated. one person

--- script.js
+++ script.js
@@ -1,2 +1,19 @@
 'use strict';
 
+const btn = document.querySelector('.btn');
+const message = document.querySelector('.message');
+const display = document.querySelector('.display');
+
+
+btn.addEventListener('click', function(e) {
+    e.preventDefault();
+
+    const textTyped = message.value;
+
+    if(!textTyped) return
+    const html = `
+    <p class="spacing"> <span class="display">${textTyped} </span></p>
+    `
+    document.querySelector('.container-1').insertAdjacentHTML('beforebegin', html);
+    message.value =''
+})
